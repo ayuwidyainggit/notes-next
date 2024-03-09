9050fc5e73fadc63e25e2b9c92b197b8063239e2
ayuwidyainggit <widyaayu560@gmail.com>
2024-03-09 16:52:23 +0700

    add layout

--- components/home/index.js
+++ components/home/index.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function GettingStarted() {
   return (
@@ -19,10 +20,11 @@ export default function GettingStarted() {
           Spare yourself the hasle of making a dozen of notes on paper and
           losing them over and over.
         </p>
-
-        <button className="text-white bg-blue-600 w-[150px] py-3 rounded-lg mt-4 hover:shadow-lg hover:bg-blue-700">
-          Start
-        </button>
+        <Link href="/notes">
+          <button className="text-white bg-blue-600 w-[150px] py-3 rounded-lg mt-4 hover:shadow-lg hover:bg-blue-700">
+            Start
+          </button>
+        </Link>
       </div>
     </>
   );
